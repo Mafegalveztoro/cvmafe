828c6be673035bcb1b045e7aef63d5fa1425372f
Mafegalveztoro <mfgalvez63533@umanizales.edu.co>
2023-05-26 19:55:56 -0500

    creacion de componente users

---
+++ src/components/users/index.js
@@ -0,0 +1,12 @@
+//=====IMPORTACION DE LIBRERIAS======
+import React from 'react';
+
+const User = () => {
+    return (
+        <div>
+            <h1>Estoy en usuarios :D</h1>
+        </div>
+    );
+};
+
+export default User;
